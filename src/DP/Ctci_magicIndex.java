ab662ada442c62e174070a5cdca0077871eeef51
rohitsingh71 <rohitsingh2359@gmail.com>
2017-12-29 02:29:31 +0530

    .

--- src/DP/Ctci_magicIndex.java
+++ src/DP/Ctci_magicIndex.java
@@ -3,20 +3,15 @@ package DP;
 /**
  * Created by rohitx71 on 29-12-2017.
  */
-public class Ctci_magicIndex
+public class Ctci_magicIndex {
 
-        public static void main(String args[]) {
-            System.out.println(fib(1000));
-           
-        }
+    public static void main(String args[]) {
+        int arr[] = {-10, 1, 1, 1, 8, 8, 30, 50, 100};
+        System.out.println(fib(1000));
 
-        static int fib(int n) {
-            int f[] = new int[n + 1];
-            f[0] = 0;
-            f[1] = 1;
-            for (int i = 2; i <= n; i++) {
-                f[i] = f[i - 1] + f[i - 2];
-            }
-            return f[n];
-        }
+    }
+
+    static int magicIndex(int n[]) {
+
+    }
 }
