dc69440cb63cf958da08badddbfdb2a6c8c7b35a
rohitx71 <rohitsingh2359@gmail.com>
2018-01-05 19:05:58 +0530

    maxsc

--- src/janLongChall/MAXSC.java
+++ src/janLongChall/MAXSC.java
@@ -9,9 +9,10 @@ public class MAXSC {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        int n = sc.nextInt();
-        sc.nextLine();
+
         while (t-- > 0) {
+            int n = sc.nextInt();
+            sc.nextLine();
             long a[][] = new long[n][n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer stringTokenizer = new StringTokenizer(sc.nextLine());
@@ -23,21 +24,28 @@ public class MAXSC {
 
             long max[] = new long[n];
             long sum = 0;
-            max[n - 1] = a[n - 1][n - 1];
+            sum = max[n - 1] = a[n - 1][n - 1];
             int[] index = new int[n];
             index[n - 1] = n - 1;
             for (int i = n - 2; i > -1; i--) {
-                for (int j = n - 1; j > 0; j--) {
+                for (int j = n - 1; j > -1; j--) {
                     if (a[i][j] < max[i + 1]) {
                         max[i] = a[i][j];
+                        sum += a[i][j];
+                        index[i] = j;
                         break;
                     }
                     if (j == n - 1) {
-                        --index[i + 1];
+                        index[i + 1]=index[i+1]-1;
+                        sum-=a[i+1][index[i+1]+1];
+
                         if (index[i + 1] == -1) {
+                            i++;
                             i++;
                             break;
                         }
+//                        System.out.println("i:" + i);
+                        sum += a[i + 1][index[i + 1]];
                         max[i + 1] = a[i + 1][index[i + 1]];
                         j = n;
 
@@ -48,11 +56,12 @@ public class MAXSC {
                     break;
                 }
             }
-            if (sum != -1)
+          /*  if (sum != -1)
                 for (int i = 0; i < n; i++) {
                     sum += max[i];
-          //          System.out.println(max[i]);
+                    //          System.out.println(max[i]);
                 }
+          */
             System.out.println(sum);
         }
     }
