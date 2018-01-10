971bb7c1c17987bdf01f58f99cb8d4b496fc7475
rohitx71 <rohitsingh2359@gmail.com>
2018-01-10 16:37:42 +0530

    chefst --done

--- src/greedy/CHEFST.java
+++ src/greedy/CHEFST.java
@@ -1,4 +1,26 @@
 package greedy;
 
+import java.util.Scanner;
+
 public class CHEFST {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long n1 = sc.nextLong();
+            long n2 = sc.nextLong();
+            long m = sc.nextLong();
+
+            long sum = (m * (m + 1)) / 2;
+            long min = Math.min(n1, n2);
+            min = Math.min(sum, min);
+
+            min = n1 + n2 - 2 * min;
+
+
+            System.out.println(min);
+
+        }
+    }
+
 }
