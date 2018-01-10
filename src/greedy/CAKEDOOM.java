4b44466e82874430fce2bf25036770121377e52f
rohitx71 <rohitsingh2359@gmail.com>
2018-01-10 16:51:13 +0530

    CAKEDOOM -C

---
+++ src/greedy/CAKEDOOM.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.util.Scanner;
+
+public class CAKEDOOM {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long n1 = sc.nextLong();
+            long n2 = sc.nextLong();
+            long m = sc.nextLong();
+
+            long sum = (m * (m + 1)) / 2;
+            long min = Math.min(n1, n2);
+            min = Math.min(sum, min);
+
+            min = n1 + n2 - 2 * min;
+
+
+            System.out.println(min);
+
+        }
+    }
+
+}
+
+}
