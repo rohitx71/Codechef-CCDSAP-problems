695c36b14b450298ac567e16b2086be059b1faf9
rohitx71 <rohitsingh2359@gmail.com>
2018-11-25 22:08:28 +0530

    CSUB++

--- src/string/CSUB.java
+++ src/string/CSUB.java
@@ -6,23 +6,15 @@ import java.util.Scanner;
  * Created by rohitx71 on 15-12-2017.
  */
 public class CSUB {
-
-
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-
-            long l = sc.nextInt();
-
-
-            String s = sc.next();
-
-            s = s.replaceAll("0", "");
-
-            long c = s.length();
+            sc.nextInt();
+            String code = sc.next();
+            code = code.replaceAll("0", "");
+            long c = code.length();
             System.out.println((c * (c + 1)) / 2);
-
         }
     }
 }
